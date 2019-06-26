ffe6e247b198e9dcbf45d61df5bffc539c58e871
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2019-06-26 11:20:49 +0200

    add GPU selection

--- sphire_janni/janni.py
+++ sphire_janni/janni.py
@@ -1,6 +1,11 @@
 import argparse
 import sys
 import json
+import os
+
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 
 
 parser = argparse.ArgumentParser(description="Just another noise to noise implementation",
@@ -9,16 +14,21 @@ subparsers = parser.add_subparsers(help='sub-command help')
 
 parser_train = subparsers.add_parser('train', help='train help')
 parser_train.add_argument('config_path', help='Path to config.json')
-
+parser_train.add_argument('-g','--gpu',type=int, default=-1, help='GPU ID to run on')
 
 parser_predict = subparsers.add_parser('predict', help='predict help')
 parser_predict.add_argument('input_path', help='Directroy / file path with images to denoise')
 parser_predict.add_argument('output_path', help='Directroy / file path to write denoised images')
 parser_predict.add_argument('model_path', help='File path to trained model')
 parser_predict.add_argument('config_path', help='File path to config.json')
+parser_predict.add_argument('-g','--gpu', type=int, default=-1, help='GPU ID to run on')
 
 def _main_():
     args = parser.parse_args()
+
+    if args.gpu != -1:
+        str_gpus = str(args.gpu)
+        os.environ["CUDA_VISIBLE_DEVICES"] = str_gpus
 
     if "train" in sys.argv[1]:
         config = read_config(args.config_path)
