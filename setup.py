69feca7451fa641578231e70c0581173b0c32fc9
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2019-12-17 13:55:20 +0100

    fix installation, release 0.1.3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Pillow >= 6.0.0",
         "Cython",
         "imagecodecs-lite==2019.2.22",
-        "tifffile==2019.7.26",
+        "tifffile==2019.7.2",
         "GooeyDev >= 1.0.3.2",
         "wxPython == 4.0.4",
     ],
